7c9e06be2598f6333880eec12f56bd19f0bf76b4
mamos <michael.amos@ubnt.com>
2019-10-28 20:49:53 -0700

    add scrollbar styles, chillscroll

--- src/components/panel/Toggle.jsx
+++ src/components/panel/Toggle.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import styled from '@emotion/styled';
+// import styled from '@emotion/styled';
 
-const StyledToggle = styled.div`
-  height: 25px;
-  width: 25px;
-  border-radius: 25px;
-  top: 25px;
-  right: -25px;
-  cursor: pointer;
-  z-index: 10;
-  background: red;
-  position: absolute;
-`;
+// const StyledToggle = styled.div`
+//   height: 25px;
+//   width: 25px;
+//   border-radius: 25px;
+//   top: 25px;
+//   right: -25px;
+//   cursor: pointer;
+//   z-index: 10;
+//   background: red;
+//   position: absolute;
+// `;
 
-export default function Toggle({ onClick }) {
-  return <StyledToggle onClick={onClick} />;
+export default function Toggle() {
+  return null;
 }
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -9,6 +9,34 @@ const globalCss = css`
     width: 100%;
     position: relative;
   }
+  * {
+    ::-webkit-scrollbar {
+      height: 20px;
+      width: 20px;
+      background-color: rgba(255, 255, 255, 0);
+    }
+
+    ::-webkit-scrollbar-track,
+    ::-webkit-scrollbar-thumb {
+      border: 8px solid rgba(255, 255, 255, 0);
+      border-radius: 10px;
+      background-clip: padding-box;
+    }
+
+    ::-webkit-scrollbar-track {
+      background-color: rgba(255, 255, 255, 0);
+    }
+
+    ::-webkit-scrollbar-thumb {
+      background-color: rgba(0, 0, 0, 0.4);
+      &:hover {
+        background-color: rgba(60, 60, 60, 0.2);
+      }
+      &:active {
+        background-color: rgba(60, 60, 60, 0.3);
+      }
+    }
+  }
 `;
 
 export default function GlobalStyles() {
