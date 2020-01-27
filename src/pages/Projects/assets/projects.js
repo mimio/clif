16f09af4a3ee075044b8c5310a6d3aef72425b01
Clifton Campbell <clif@mimio.io>
2020-01-26 19:30:28 -0800

    add more projects

--- src/pages/Projects/assets/projects.js
+++ src/pages/Projects/assets/projects.js
@@ -1,12 +1,15 @@
 import harvard from './harvard.png';
 import ngwsd from './ngwsd.png';
 import polygoggles from './polygoggles.png';
+import sage from './sage.png';
+import gopro from './gopro.png';
+import bop from './bop.png';
 
 export default [
   {
     title: 'Gentrification Analysis Tool',
     subtitle:
-      'We worked alongside Harvard to construct this scientific mapping tool that displays social and economic changes across the Greater Boston area starting in 1990.',
+      'I worked alongside Harvard to construct this scientific mapping tool that displays social and economic changes across the Greater Boston area starting in 1990.',
     imgSrc: harvard,
     href:
       'https://www.jchs.harvard.edu/boston-map#/boston-map/create-map',
@@ -15,18 +18,42 @@ export default [
   {
     title: 'Sports Events Finder',
     subtitle:
-      "The The Women's Sports Foundation asked us to create a tool for users to find and research events in their area for the National Girls & Women in Sports Day.",
+      "The The Women's Sports Foundation asked me to create a tool for users to find and research events in their area for the National Girls & Women in Sports Day.",
     imgSrc: ngwsd,
     href:
       'https://www.womenssportsfoundation.org/get-involved/ngwsd/',
     key: 'ngwsd',
   },
   {
+    title: 'GoPro Mountain Games Event Map',
+    subtitle:
+      'I worked with 970 Design to create an annual event map for users to navigate the GoPro Mountain Games.',
+    imgSrc: gopro,
+    href: 'https://mountaingames.com/map.php',
+    key: 'gopro',
+  },
+  {
     title: '3D Asset Searching and Viewing Tool (Augmented Reality)',
     subtitle:
-      'We worked with Deadlock Interactive to create a 3D asset browsing and viewing platform, with augmented reality capabilities on mobile browsers.',
+      'I worked with Deadlock Interactive to create a 3D asset browsing and viewing platform, with augmented reality capabilities on mobile browsers.',
     imgSrc: polygoggles,
     href: 'http://poly-goggles.herokuapp.com/',
     key: 'poly',
+  },
+  {
+    title: 'Interactive Trailmap',
+    subtitle:
+      "I worked with 970 Design and Sage Outdoor Adventures to construct a trailmap with interactive areas, lines, and points to help users navigate the client's terrain as well as show off the large amount of land and activites offered.",
+    imgSrc: sage,
+    href: 'https://sageoutdooradventures.com/map/',
+    key: 'sage',
+  },
+  {
+    title: 'Birds of Prey Winter Sports Event Map',
+    subtitle:
+      '970 Design and I iterated on our work for the GoPro Mountain Games to create an annual event map for the BoP World Cup.',
+    imgSrc: bop,
+    href: 'https://bcworldcup.com/map.php',
+    key: 'bop',
   },
 ];
