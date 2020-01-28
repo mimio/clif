f6e580ae20e580db8a91b2e4377a3cc5c94c90b5
Clifton Campbell <clif@mimio.io>
2020-01-28 10:32:33 -0800

    rmv unused selectors

--- src/modules/app/selectors.js
+++ src/modules/app/selectors.js
@@ -3,14 +3,6 @@ import { createSelector } from 'reselect';
 
 export const selectState = state => state.app;
 
-export const selectProgress = createSelector(selectState, app =>
-  get(app, 'progress'),
-);
-
-export const selectSelectedTab = createSelector(selectState, app =>
-  get(app, 'selectedTab'),
-);
-
 export const selectIsCursorActive = createSelector(selectState, app =>
   get(app, 'isCursorActive'),
 );
