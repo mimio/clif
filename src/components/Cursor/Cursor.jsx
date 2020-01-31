690b53aae65df9d438b588ee5fd4291dff54f54d
Clifton Campbell <clif@mimio.io>
2020-01-30 17:23:50 -0800

    refactor isTouchScreen

--- src/components/Cursor/Cursor.jsx
+++ src/components/Cursor/Cursor.jsx
@@ -12,19 +12,11 @@ const isTargetActive = target =>
   isElementActive(get(target, 'parentElement.parentElement'));
 
 const isTouchScreen = () => {
-
   const prefixes = ' -webkit- -moz- -o- -ms- '.split(' ');
-
-  const mq = function (query) {
-      return window.matchMedia(query).matches;
-  };
-
+  const mq = query => window.matchMedia(query).matches;
   if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
       return true;
   }
-
-  // include the 'heartz' as a way to have a non matching MQ to help terminate the join
-  // https://git.io/vznFH
   const query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('');
   return mq(query);
 };
