90a5b3db946a3d34b0b07c095a84ee9cb81084bc
Clifton Campbell <clif@mimio.io>
2020-05-10 12:05:02 -0700

    increase in zoom

--- modules/map/config.js
+++ modules/map/config.js
@@ -3,7 +3,7 @@ import { BOUNDS_PADDING } from 'constants/map';
 export const config = {
   accessToken:
     'pk.eyJ1IjoiY2hpZWZrbGVlZiIsImEiOiJjaWhkbnE5cGEwYnltdnFrbHBwaHd0NXhuIn0.SXxGsE9D61dU7gWmWEV71Q',
-  minZoom: 11,
+  minZoom: 10,
   maxZoom: 12,
   bearing: 0,
   pitch: 120,
